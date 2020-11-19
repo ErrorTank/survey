fbcf2651731bdddfa869a8723ad06b6e0982ee0f
Tuan Anh <ncq998@gmail.com>
2020-11-19 11:01:04 +0700

    dá321

--- server/express.js
+++ server/express.js
@@ -21,7 +21,7 @@ module.exports = () => {
             "Origin, X-Requested-With, Content-Type, Accept, Authorization"
         );
         if (req.method === "OPTIONS") {
-            res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+            res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET, OPTIONS");
             return res.status(200).json({});
         }
         next();

e88d1a7f233b0ffa26cc9d5ec835aeebd0ddc777
Tuan Anh <ncq998@gmail.com>
2020-11-19 11:04:48 +0700

    dá321

--- server/express.js
+++ server/express.js
@@ -26,6 +26,12 @@ module.exports = () => {
         }
         next();
     });
+    app.options("/*", function(req, res, next){
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+        res.send(200);
+    });
     app.use("/", (req, res, next) => {
 
         return express.static(process.cwd() + "/" + process.env.STATIC_DIR)(req, res, next)
